c8e52abc42398a70af590d70e487ed7428350d13
JMaylor <60825814+JMaylor@users.noreply.github.com>
2022-02-05 15:24:16 +0000

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: "class",
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        primary: "#37B4E2",
+        secondary: colors.sky["700"],
+        accent: "#F48120",
+      },
+    },
+  },
   plugins: [
     require("@tailwindcss/line-clamp"),
     // ...
