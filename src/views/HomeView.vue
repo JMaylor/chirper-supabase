615b17b657a8384b322eec9977c219c2d50a5b80
JMaylor <60825814+JMaylor@users.noreply.github.com>
2022-02-06 13:09:28 +0000

    realtime chirp inserts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,6 @@ import { ChirpWithAuthor } from "@/types";
 const chirps = ref([] as ChirpWithAuthor[]);
 async function fetchChirps() {
   const { supabase } = useAuthStore();
-
   const { data, error } = await supabase
     .from("chirps")
     .select(
@@ -25,11 +24,37 @@ async function fetchChirps() {
   if (data) chirps.value = data;
 }
 
-fetchChirps();
+fetchChirps().then(() => {
+  const { supabase } = useAuthStore();
+  supabase
+    .from("chirps")
+    .on("*", async (payload: any) => {
+      if (payload.eventType == "INSERT") {
+        const { data, error } = await supabase
+          .from("chirps")
+          .select(
+            `
+              id,
+              body,
+              created_at,
+              author (
+                handle,
+                user_name,
+                picture
+              )
+            `
+          )
+          .eq("id", payload.new.id);
+        if (error) alert(error);
+        if (data) chirps.value.unshift(...data);
+      }
+    })
+    .subscribe();
+});
 </script>
 
 <template>
-  <div class="flex flex-col border-b divide-y">
+  <div class="flex flex-col divide-y border-b">
     <FeedChirp v-for="chirp in chirps" :chirp="chirp"></FeedChirp>
   </div>
 </template>
