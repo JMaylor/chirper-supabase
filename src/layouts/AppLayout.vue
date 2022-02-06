da75a0084a2db9f8827942694c86f0b42d25041b
JMaylor <60825814+JMaylor@users.noreply.github.com>
2022-02-06 11:35:56 +0000

    remove comments

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -3,29 +3,15 @@ import PostChirpWidget from "@/components/PostChirpWidget.vue";
 </script>
 
 <template>
-  <!-- <div class="h-full w-full">
-    <div class="relative mx-auto h-full max-w-2xl">
-      <AppDrawer />
-
-      <div
-        class="flex h-full max-h-full w-full flex-col pb-16 sm:pb-0 sm:pl-16"
-      >
-        <AppHeader />
-        <div class="flex-grow overflow-y-scroll">
-          <PostChirpWidget />
-          <router-view />
-        </div>
-      </div>
-    </div>
-  </div> -->
-
   <div
     class="mx-auto flex h-full max-h-full w-full flex-col justify-center sm:flex-row-reverse"
   >
-    <div class="max-w-screen-sm flex-grow overflow-y-scroll sm:overflow-y-auto border">
+    <div
+      class="max-w-screen-sm flex-grow overflow-y-scroll border sm:overflow-y-auto"
+    >
       <div class="sticky top-0 border-b"><AppHeader /></div>
       <div class="overflow-x-auto">
-        <PostChirpWidget class="border-b"/>
+        <PostChirpWidget class="border-b" />
         <router-view />
       </div>
     </div>
